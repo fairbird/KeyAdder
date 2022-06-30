4a271e44cced8b381c708ca559cc62d74cfeacaf
RAED <rrrr53@hotmail.com>
2022-06-30 15:08:41 +0300

    [installer.sh] Replace sh with bash

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/fairbird/KeyAdder/main/installer.sh -O - | /bin/sh
 
 ######### Only These two lines to edit with new version ######
