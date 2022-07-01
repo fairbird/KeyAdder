3c6eaf77521d20a7572ff48b7803e3ee64f63a2a
MOHAMED_OS <47327706+MOHAMED19OS@users.noreply.github.com>
2022-07-01 21:27:11 +0100

    update script install KeyAdder
    Co-authored-by: MOHAMED19OS <mohamed_os@gmail.com>

--- installer.sh
+++ installer.sh
@@ -1,79 +1,102 @@
 #!/bin/bash
-##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/fairbird/KeyAdder/main/installer.sh -O - | /bin/sh
+# ###########################################
+# SCRIPT : DOWNLOAD AND INSTALL KeyAdder
+# ###########################################
+#
+# Command: wget https://raw.githubusercontent.com/fairbird/KeyAdder/main/installer.sh -qO - | /bin/sh
+#
+# ###########################################
 
 ######### Only These two lines to edit with new version ######
 version=5.8
 description=What is NEW:\n[[Move plugin to my source github]\nما هو الجديد:\n[github نقل البلجن الى سورساتي على]
 ##############################################################
 
-# Python version
-PYTHON_VERSION=$(python -c"import platform; print(platform.python_version())")
+###########################################
+# Configure where we can find things here #
+TMPDIR='/tmp'
+PLUGINDIR='/usr/lib/enigma2/python/Plugins/Extensions'
 
-# remove old version
-rm -rf /usr/lib/enigma2/python/Plugins/Extensions/AddKey
-rm -rf /usr/lib/enigma2/python/Plugins/Extensions/KeyAdder
-rm -rf /usr/lib/enigma2/python/Plugins/Extensions/Biscotto
+#######################
+# Remove Old Version #
+rm -rf $PLUGINDIR/AddKey
+rm -rf $PLUGINDIR/KeyAdder
+rm -rf $PLUGINDIR/Biscotto
+rm -rf $TMPDIR/*main*
 
-# Download and install plugin
-
-if [ -f /var/lib/dpkg/status ]; then
-   STATUS=/var/lib/dpkg/status
-   OSTYPE=DreamOs
-else
-   STATUS=/var/lib/opkg/status
-   OSTYPE=Dream
+#########################
+if [ -f /etc/opkg/opkg.conf ]; then
+    STATUS='/var/lib/opkg/status'
+    OSTYPE='Opensource'
+    OPKG='opkg update'
+    OPKGINSTAL='opkg install'
+elif [ -f /etc/apt/apt.conf ]; then
+    STATUS='/var/lib/dpkg/status'
+    OSTYPE='DreamOS'
+    OPKG='apt-get update'
+    OPKGINSTAL='apt-get install'
 fi
 
-if python --version 2>&1 | grep -q '^Python 3\.'; then
-	echo "You have Python3 image"
-	PYTHON=PY3
-	Packagesix=python3-six
+#########################
+install() {
+    if grep -qs "Package: $1" $STATUS; then
+        echo
+    else
+        $OPKG >/dev/null 2>&1
+        echo "   >>>>   Need to install $1   <<<<"
+        echo
+        if [ $OSTYPE = "Opensource" ]; then
+            $OPKGINSTAL "$1"
+            sleep 1
+            clear
+        elif [ $OSTYPE = "DreamOS" ]; then
+            $OPKGINSTAL "$1" -y
+            sleep 1
+            clear
+        fi
+    fi
+}
+
+#########################
+if [ "${PYTHON_VERSION}" == 3 ]; then
+    echo ":You have Python3 image ..."
+    sleep 1
+    Packagesix=python3-six
 else
-	echo "You have Python2 image"
-	PYTHON=PY2
-	Packagesix=python-six
+    echo ":You have Python2 image ..."
+    sleep 1
+    Packagesix=python-six
 fi
 
-if grep -qs "Package: $Packagesix" cat $STATUS ; then
-	echo ""
-else
-	echo "Need to install $Packagesix"
-	echo ""
-	if [ $OSTYPE = "DreamOs" ]; then
-		apt-get update && apt-get install python-six -y
-	elif [ $PYTHON = "PY3" ]; then
-		opkg update && opkg install python3-six
-	elif [ $PYTHON = "PY2" ]; then
-		opkg update && opkg install python-six
-	fi
-fi
-echo ""
+install $Packagesix
 
-cd /tmp
+#########################
 set -e
-wget -q "--no-check-certificate" https://github.com/fairbird/KeyAdder/archive/refs/heads/main.tar.gz
-tar -xf main.tar.gz
-cp -r KeyAdder-main/usr /
-rm -rf *main*
+echo "Downloading And Insallling KeyAdder plugin Please Wait ......"
+echo
+wget --show-progress https://github.com/fairbird/KeyAdder/archive/refs/heads/main.tar.gz -qP $TMPDIR
+tar -xzf $TMPDIR/main.tar.gz
+cp -r $TMPDIR/KeyAdder-main/usr /
 set +e
-cd ..
-sync
 
-### Check if plugin installed correctly
-if [ ! -d '/usr/lib/enigma2/python/Plugins/Extensions/KeyAdder' ]; then
-	echo "Some thing wrong .. Plugin not installed"
-	exit 1
-fi
+#########################
+rm -rf $TMPDIR/*main*
 
-sync
+sleep 1
+clear
 echo "#########################################################"
 echo "#          KeyAdder INSTALLED SUCCESSFULLY              #"
-echo "#                 Raed  &  mfaraj57                     #"              
+echo "#                 Raed  &  mfaraj57                     #"
 echo "#                     support                           #"
 echo "#   https://www.tunisia-sat.com/forums/threads/3955125/ #"
 echo "#########################################################"
 echo "#           your Device will RESTART Now                #"
 echo "#########################################################"
-sleep 3
-killall enigma2
+
+if [ $OSTYPE = "Opensource" ]; then
+    killall -9 enigma2
+else
+    systemctl restart enigma2
+fi
+
 exit 0
