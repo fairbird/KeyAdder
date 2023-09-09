538b2f8566da1dbcb52bbaca506b84ec9c4efeec
RAED <rrrr53@hotmail.com>
2023-09-09 19:04:58 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -8,8 +8,8 @@
 # ###########################################
 
 ######### Only These two lines to edit with new version ######
-version='7.3'
-description='What is NEW:\n- Remove MOHAMED_OS from list of download softcams file. No more link download.\n\n*************************\n:ما هو الجديد\n- من قائمة تحميل ملفات السوفتكام لعدم توفره MOHAMED_OS حذف عنوان'
+version='7.4'
+description='What is NEW:\n- Add Novaler4k to list of download softcams file.\n\n*************************\n:ما هو الجديد\n- لقائمة تحميل ملفات السوفتكام Novaler4k إضافة عنوان'
 ##############################################################
 
 ###########################################
